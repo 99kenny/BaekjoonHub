c7cd6ce8f245921b59ff610a5bbf97f2b257c838
99kenny <57697721+99kenny@users.noreply.github.com>
2022-08-19 16:14:50 +0900

    [Silver IV] Title: 동전 0, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -7,10 +7,9 @@ for i in range(0,coinNum):
     value.append(int(input()))
 
 count = 0
-total = amount
 value.reverse()
 
 for i in value:
-    count += (total // i)
-    total = total % i
+    count += (amount // i)
+    amount = amount % i
 print(count)        
